b3be193280c76ad2842b9d3cf8fbdb8fa303cc46
bogibekdev <bogibekdev@gmail.com>
2023-03-02 20:16:43 +0500

    fixed

--- app/src/main/java/dev/bogibek/drawablemenu/MainActivity.kt
+++ app/src/main/java/dev/bogibek/drawablemenu/MainActivity.kt
@@ -1,19 +1,17 @@
 package dev.bogibek.drawablemenu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import dev.bogibek.drawablemenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupDrMenu()
     }
@@ -23,12 +21,11 @@ class MainActivity : AppCompatActivity() {
             ivMenu.setOnClickListener {
                 drawerLayout.openDrawer(GravityCompat.START)
             }
-            navigationView.itemIconTintList=null
-            val navController=Navigation.findNavController(this@MainActivity,R.id.navHostFragment)
-            NavigationUI.setupWithNavController(navigationView,navController)
-            navController.addOnDestinationChangedListener { controller, destination, arguments ->
-                tvName.text = destination.label
-            }
+            navigationView.itemIconTintList = null
+            val navController =
+                (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment).navController
+            NavigationUI.setupWithNavController(navigationView, navController)
+
         }
     }
 }
