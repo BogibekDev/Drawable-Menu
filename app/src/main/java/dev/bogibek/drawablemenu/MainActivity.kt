8ccbe3874614f4fa68a53b58a7d43d91ad5a1a47
bogibekdev <bogibekdev@gmail.com>
2023-03-02 19:55:03 +0500

    first commit

---
+++ app/src/main/java/dev/bogibek/drawablemenu/MainActivity.kt
@@ -0,0 +1,41 @@
+package dev.bogibek.drawablemenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import dev.bogibek.drawablemenu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupDrMenu()
+    }
+
+    private fun setupDrMenu() {
+        binding.apply {
+            ivMenu.setOnClickListener {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+            navigationView.itemIconTintList=null
+            val navController=Navigation.findNavController(this@MainActivity,R.id.navHostFragment)
+            NavigationUI.setupWithNavController(navigationView,navController)
+            navController.addOnDestinationChangedListener(object :NavController.OnDestinationChangedListener{
+                override fun onDestinationChanged(
+                    controller: NavController,
+                    destination: NavDestination,
+                    arguments: Bundle?
+                ) {
+                    tvName.text=destination.label
+                }
+
+            })
+        }
+    }
+}
